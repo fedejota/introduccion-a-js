baa6c8feb6121ed50a7dbd87161f35db9b66e600
fedejota <fedejota@gmail.com>
2019-10-15 22:53:12 -0300

    Prueba git-github
    Estoy probando el uso de git y github, me costó entenderlo y quiero ver si logro hacer un pull request y llegue a Argentina Programa

---
+++ tareas/clase-5/tarea-clase-5 v.fedejota.js
@@ -0,0 +1,26 @@
+/*
+/*
+
+Esto es una prueba.
+
+Estoy probando git-github. Me costó entender esto. Me parece que me faltaba hacer un fork previamente a clonar y 
+como clonaba el original cuando modificaba y publicaba decía que no tenía autorización.
+
+Pasos realizados:
+
+1.- Fork de intro a JS
+2.- Clonado como mi repositorio (¿cuando se modifique el original se modificará mi fork clonado?)
+3.- Abierto el repositorio desde vs code
+3.- Copiado archivo de tarea clase 5 en este archivo y modificado
+
+Próximos pasos:
+
+1.- Hacer un branch y un commit en el branch
+2.- Sincronizar con github web
+2.- Hacer un pull request para probar 
+3.- Ver que pasa, si le llega a alguien y si me contestan.
+
+Si esto llega a Fabricio o a alguien por favor contestar así veo si los pasos realizados fueron los correctos.
+
+*/
+
